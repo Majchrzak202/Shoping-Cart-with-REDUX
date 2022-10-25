226dd3d453dc96142a3cbc1651d749555bc5b34e
MarcinMaster <m.luba505@gmail.com>
2022-10-25 08:55:05 +0200

    Added a Cart, Footer, Navbar, Home and Layout components

---
+++ src/components/Cart.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <div style={{height: '100vh'}}>Cart</div>
+  )
+}
+
+export default Cart
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { BottomNavigation } from "@mui/material";
+
+const Footer = () => {
+  return <BottomNavigation showLabels></BottomNavigation>;
+};
+
+export default Footer;
