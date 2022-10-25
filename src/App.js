4ae3de928816a32d091d92f71cf2d07bb3ae0e49
MarcinMaster <m.luba505@gmail.com>
2022-10-25 08:50:31 +0200

    Added a Layout which holds a Navbar and Footer and also internal background. Also added Routes to Home and Cart Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+import Layout from "./components/Layout";
 
 const App = () => {
-  return <div className="App">Japa</div>;
+  return (
+    <div>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </Layout>
+    </div>
+  );
 };
 
 export default App;
