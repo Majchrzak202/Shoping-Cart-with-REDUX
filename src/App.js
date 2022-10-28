0128cae073efaf115042a7630e795d788347784e
MarcinMaster <m.luba505@gmail.com>
2022-10-28 08:55:01 +0200

    Added Toast Container and tostify css file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import 'react-toastify/dist/ReactToastify.css';
 import { Routes, Route } from "react-router";
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 import Layout from "./components/Layout";
+import { ToastContainer } from "react-toastify";
+
 
 const App = () => {
   return (
     <div>
       <Layout>
+        <ToastContainer/>
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
